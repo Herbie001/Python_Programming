0d5f2b47411d533177295e876caa5402f2c56623
Eric Hebert <ehebert@ucsd.edu>
2025-04-04 15:52:18 -0700

    Added trivial fdirectory for examples

---
+++ Trivial/helloWorld.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+
+message = "This is a message with no type"
+
+print(message + ", concatenating message")
+print(message.title())
+print(message.upper())
+print(message.lower())
